e1ab7cd96e347cfeb247fe0b84d2767d9771372d
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-01-13 14:14:07 +0100

    Import API

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,44 @@
 <script >
+import { store } from './store.js';
+import axios from 'axios';
+
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 
 
 export default {
   name: 'App',
+  components: { AppHeader, AppMain },
 
-  components: { AppHeader, AppMain }
+  data() {
+    return {
+      store,
+      apiUrl: 'https://api.themoviedb.org/3/search/movie?'
+    }
+  },
+
+  methods: {
+    getFilmApi(importUrl) {
+
+      axios.get(importUrl, {
+        params: {
+          api_key: '16eea8fe6e93b980795f69e2abba8997',
+          language: 'it-IT',
+          query: 'a' //for research
+        }
+      })
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    },
+  },
+
+  created() {
+    this.getFilmApi(this.apiUrl)
+  },
 }
 </script>
 
