2d4e0a675aa694e1468f5927daf38378e790f22d
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-01-16 18:06:11 +0100

    Create Popular Film structure

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,21 @@
 <script>
+import { store } from '../store.js';
+
 import FilmList from './FilmList.vue';
+import PopularFilm from './PopularFilm.vue';
+
 
 
 export default {
     name: 'AppMain',
     components: {
-        FilmList
+        FilmList,
+        PopularFilm
+    },
+    data() {
+        return {
+            store
+        }
     },
 
 }
@@ -13,8 +23,10 @@ export default {
 
 <template>
     <main>
+        <!-- Show Popular Film -->
+        <PopularFilm v-if="store.filmList.length == 0" />
         <!-- Generate Film List -->
-        <FilmList />
+        <FilmList v-else />
     </main>
 </template>
 
---
+++ src/components/PopularFilm.vue
@@ -0,0 +1,29 @@
+<script>
+import SingleFilmInfo from './SingleFilmInfo.vue'
+
+
+export default {
+    name: 'PopularFilm',
+    components: {
+        SingleFilmInfo
+    }
+}
+</script>
+
+<template>
+    <section id="popular-film">
+        <h2>Film Popolari</h2>
+
+    </section>
+</template>
+
+<style lang="scss">
+section#popular-film {
+    padding: 2rem;
+
+    h2 {
+        font-size: 3rem;
+        color: white;
+    }
+}
+</style>
