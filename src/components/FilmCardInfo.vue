aed7a89cbd7e9cc3423c93d3ed2139fbaad9e88f
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-01-16 15:51:20 +0100

    Create components for film info

---
+++ src/components/FilmCardInfo.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    name: 'FilmCardInfo',
+    props: ['singleFilm'],
+
+}
+</script>
+
+<template lang="">
+    <div class="film-info">
+
+        <h3>Titolo: {{ singleFilm.title || singleFilm.name }} </h3>
+        <h4>Titolo originale: {{ singleFilm.original_title || singleFilm.original_name }}</h4>
+        <img :src="`https://crowdin.com/images/flags/${singleFilm.original_language}.png`"
+        :alt="singleFilm.original_language + ' flag'" onerror="this.style.display='none'">
+        <br>
+        
+        <div class="star-rate">
+            <span>Voto: </span>
+            <font-awesome-icon icon="star" class="vote" v-for="n in Math.ceil(singleFilm.vote_average / 2)" />
+            <font-awesome-icon icon="star" v-for="n in (5 - Math.ceil(singleFilm.vote_average / 2))" />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+div.star-rate {
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+
+    .vote {
+        color: orange;
+    }
+}
+</style>
--- src/components/SingleFilmInfo.vue
+++ src/components/SingleFilmInfo.vue
@@ -1,8 +1,12 @@
 <script>
+import FilmCardInfo from './FilmCardInfo.vue';
 
 export default {
     name: 'SingleFilmInfo',
     props: ['singleFilm'],
+    components: {
+        FilmCardInfo
+    },
     data() {
         return {
             posterUrl: 'https://image.tmdb.org/t/p/w342',
@@ -19,28 +23,12 @@ export default {
             onerror="this.style.display='none'">
     </div>
 
-    <h3>Titolo: {{ singleFilm.title || singleFilm.name }} </h3>
-    <h4>Titolo originale: {{ singleFilm.original_title || singleFilm.original_name }}</h4>
-    <img :src="`https://crowdin.com/images/flags/${singleFilm.original_language}.png`"
-        :alt="singleFilm.original_language + ' flag'" onerror="this.style.display='none'">
-    <br>
+    <FilmCardInfo :singleFilm="singleFilm" />
 
-    <!-- <span>Voto: {{ Math.ceil(singleFilm.vote_average / 2) }}</span> -->
-    <div class="star-rate">
-        <font-awesome-icon icon="star" class="vote" v-for="n in Math.ceil(singleFilm.vote_average / 2)" />
-        <font-awesome-icon icon="star" v-for="n in (5 - Math.ceil(singleFilm.vote_average / 2))" />
-    </div>
 
 
 </template>
 
 <style lang="scss" scoped>
-div.star-rate {
-    font-size: 1.5rem;
-    margin-bottom: 2rem;
 
-    .vote {
-        color: orange;
-    }
-}
 </style>
